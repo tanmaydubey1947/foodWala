3cce87fcdbef33ffcd061d9ed548a21c554ecdea
TANMAY DUBEY <48459376+tanmaydubey1947@users.noreply.github.com>
2022-11-16 18:05:47 +0530

    updated code structure.

--- src/main/java/com/restaurant/foodWala/dao/CustomerDetailsDto.java
+++ src/main/java/com/restaurant/foodWala/dao/CustomerDetailsDto.java
@@ -2,30 +2,34 @@ package com.restaurant.foodWala.dao;
 
 public class CustomerDetailsDto {
 
-		public int customerId;
-		public String customerName;
-		public CustomerDetailsDto() {
-			super();
-			// TODO Auto-generated constructor stub
-		}
-		public CustomerDetailsDto(int customerId, String customerName) {
-			super();
-			this.customerId = customerId;
-			this.customerName = customerName;
-		}
-		public int getCustomerId() {
-			return customerId;
-		}
-		public void setCustomerId(int customerId) {
-			this.customerId = customerId;
-		}
-		public String getCustomerName() {
-			return customerName;
-		}
-		public void setCustomerName(String customerName) {
-			this.customerName = customerName;
-		}
-		
-		
-	
+	public int customerId;
+	public String customerName;
+
+	public CustomerDetailsDto() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public CustomerDetailsDto(int customerId, String customerName) {
+		super();
+		this.customerId = customerId;
+		this.customerName = customerName;
+	}
+
+	public int getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+
+	public String getCustomerName() {
+		return customerName;
+	}
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
 }
